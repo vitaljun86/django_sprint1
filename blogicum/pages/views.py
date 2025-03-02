83ac41b88b5ab8dbfa196a2d8089df593cf77949
Vitaly Panfilov <vitosikp@yandex,ru>
2025-03-02 23:02:35 +0300

    new version

--- blogicum/pages/views.py
+++ blogicum/pages/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def about(request):
+    templates = 'pages/about.html'
+    return render(request, templates)
+
+def rules(request):
+    templates = 'pages/rules.html'
+    return render(request, templates)
